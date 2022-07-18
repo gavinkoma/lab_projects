9d0647666b9055df3acf2b979fa0a1f43975711a
Gavin Koma <gavintkoma@gmail.com>
2022-07-18 17:44:50 -0400

    gui now has buttons, publishes to terminal, and offers topic support

--- src/tutorial/src/simple_gui.py
+++ src/tutorial/src/simple_gui.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-
+import queue
 import rospy
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from std_msgs.msg import Bool
 
 
 #kivy convention is that you can name your kivy class literally anything you want but
@@ -21,7 +22,45 @@ class TutorialApp(MDApp):
     def build(self):
         return self.screen
 
+    def start_tread(self,*args):
+        print("Started Treadmill.")
+
+    def record_video(self,*args):
+        print("Recording started.")
+
+    def stop_exit(self,*args):
+        print("Quitting and exiting.")
+
+    def speed_16(self,*args):
+        print("Setting treadmill speed to 16.")
+
+    def speed_20(self,*args):
+        print("Setting treadmill speed to 20.")
+
+    def speed_24(self,*args):
+        print("Setting treadmill speed to 24.")
+
+    def speed_28(self,*args):
+        print("Setting treadmill speed to 28.")
+
+    def speed_32(self,*args):
+        print("Setting treadmill speed to 32.")
+
 if __name__ == '__main__':
+    #okay so we have 16, 20, 24, 28, 32 speeds and we need a button for every one of them
+    #we also will need a button that simple starts and stops the treadmill
+    #we could include a window for camera view but i havent a clue how to do that,
+    #(and it feels complicated)
+    pub_start=rospy.Publisher('/start_tread',Bool,queue_size=1)
+    pub_record=rospy.Publisher('/record',Bool,queue_size=1)
+    pub_exit=rospy.Publisher('/exit',Bool,queue_size=1)
+
+    pubsp16=rospy.Publisher('sp16',Bool,queue_size=1)
+    pubsp20=rospy.Publisher('sp20',Bool,queue_size=1)
+    pubsp24=rospy.Publisher('sp24',Bool,queue_size=1)
+    pubsp28=rospy.Publisher('sp28',Bool,queue_size=1)
+    pubsp32=rospy.Publisher('sp32',Bool,queue_size=1)
+
 
     rospy.init_node('simple_gui', anonymous=True)
 
