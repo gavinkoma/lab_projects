a8ce31799af320a690e9fe0bd59f85469597df39
Gavin Koma <gavintkoma@gmail.com>
2023-06-15 12:06:05 -0400

    multiindex + readme updated

--- ajay_kinematics/README.md
+++ ajay_kinematics/README.md
@@ -2,3 +2,31 @@
 .ignore included to ignore ajay file data
 lets do a bit of exploring of the data 
 need to chat with siya about how we want to work through the data
+
+---
+
+06.15.2023
+we want to calculate:
+	1. additional features
+	2. height --> min, max, average?
+	3. segment angles (diff from joint angles)
+	4. markers/joints (is there a difference?) 
+
+we will probably want to calculate some variable and compare with qda (we can try lda but qda is normally better with nonlinear data) and then toss it into a random forest model! wooooo 
+
+we will definitely want to perform some version of a clustering algo. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+-----
+
--- ajay_kinematics/eda.py
+++ ajay_kinematics/eda.py
@@ -5,35 +5,42 @@ idx = pd.IndexSlice
 
 #lets view and assess the data first 
 ajay = pd.read_hdf("/Users/gavinkoma/Documents/lab_projects/ajay_kinematics/data/alldata.h5")
-ajay = ajay.iloc[3:]
+#ajay = ajay.iloc[3:]
 #view as below, see head:
 print(ajay.head(4))
 #we have collarbone --> shoulder --> elbow --> wrist 
-datadir='../data'
-ds = ajay
-ds.index=ds.index.set_names(['video','frame'])
+#lets take a second and see what keys were working with
+for k in ajay.keys():
+	print(k)
 
-dsj = ds.groupby(['video'],group_keys=False)
+#the colon behind collarbone means all scoreers 
+first = ajay.loc[idx['2023-05-25_10_25_46',:],idx[:,'Collarbone',['x','y']]]
+elb = ajay.loc[idx[:,:],idx[:,'Elbow',['x','y']]].droplevel(["scorer","bodyparts"],axis='columns')
+wri = ajay.loc[idx[:,:],idx[:,'Wrist',['x','y']]].droplevel(["scorer","bodyparts"],axis='columns')
 
 
-# get intersection of videos in metadata and dataset
-#vids=list(set(ds.index) & set(ds.index.get_level_values('video').unique()))
+feat_vec = {}
+
+
+euc_dist = (elb-wri).rename(columns={"x":"dx",
+									"y":"dy"})
+
+feat_vec.update(euc_dist)
+
+
+
+first.index.get_level_values('video').unique()
 
 #alright lets make a few plots i guess, would be worth it to assess what were looking at 
 #we can groupby video name and then plot those for our assessment 
 #probably will take a good bit though just because of the overall size
-
 def create_plots(x,y):
-	#we will want a line plot that shows the x and y coordinates
-	#and we will want to calculate the angle around the elbow 
-	#maybe calculate the angle around the shoulder as well 
-	#height? velocity? distance? 
 	pass 
 
 def calc_vel():
 	pass
 
-def calc_heigh():
+def calc_height():
 	pass
 
 
