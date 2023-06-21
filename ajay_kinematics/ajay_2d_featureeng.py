bbee26654042ad03287ae5a5b35df5210122cc20
Gavin Koma <gavintkoma@gmail.com>
2023-06-21 15:18:28 -0400

    updated topics

--- ajay_kinematics/ajay_2d_featureeng.py
+++ ajay_kinematics/ajay_2d_featureeng.py
@@ -89,64 +89,75 @@ shou_vector_y_ = -np.diff(shou.iloc[:,1])
 
 joint_angle_elbow(wri,elb,shou)
 
-#segment angles are in reference to the ground ! 
-#we need a line that can function as the reference to the ground 
-#lets suppose that the following works:
-#it would be nice to make this kind of dynamic lol 
-
-#keep shoulder and elbow line the same 
-#make a new line from elbow to maybe like 50 points over
-#we want the x coordinate to change and leave the y coordinate to be same
-#lets create these new lists for these data points pls 
-
 #list your inputs from top to bottom, this is important because this 
 #is how i want to design this function
 
-def reference_line(extend):
-	reference_line_elbow = []
-	reference_elbow = pd.DataFrame()
-	for val in extend.iloc[:,0]:
-		x_val = val + 50
-		#print(x_val)
-		reference_line_elbow.append(x_val)
-	extend['ref_x'] = reference_line_elbow
+# def prep(extend):
+# 	reference_line_elbow = []
+# 	reference_elbow = pd.DataFrame()
+# 	for val in extend.iloc[:,0]:
+# 		x_val = val
+# 		#print(x_val)
+# 		reference_line_elbow.append(x_val)
+# 	extend['ref_x'] = reference_line_elbow
 
-	reference_line_elbow = []
-	for val in extend.iloc[:,1]:
-		y_val = val
-		#print(x_val)
-		reference_line_elbow.append(y_val)
-	extend['ref_y'] = reference_line_elbow
-	return extend
-
-shoulder_new = reference_line(shou)
-shoulder_new.drop(shoulder_new.columns[[0, 1]], axis=1, inplace=True)
-elb_new = reference_line(elb)
-elb_new.drop(elb_new.columns[[0, 1]], axis=1, inplace=True)
-wrist_new = reference_line(wri)
-wrist_new.drop(wrist_new.columns[[0, 1]], axis=1, inplace=True)
-wri = ajay.loc[idx['2023-05-25_10_25_46',:],idx[:,'Wrist',['x','y']]].droplevel(["scorer","bodyparts"],axis='columns')
-elb = ajay.loc[idx['2023-05-25_10_25_46',:],idx[:,'Elbow',['x','y']]].droplevel(["scorer","bodyparts"],axis='columns')
-shou = ajay.loc[idx['2023-05-25_10_25_46',:],idx[:,'Shoulder',['x','y']]].droplevel(["scorer","bodyparts"],axis='columns')
+# 	reference_line_elbow = []
+# 	for val in extend.iloc[:,1]:
+# 		y_val = val
+# 		#print(x_val)
+# 		reference_line_elbow.append(y_val)
+# 	extend['ref_y'] = reference_line_elbow
+# 	return extend
 
 #our new issue is that the reference line will give us a value of 0 and that literally makes 
 #taking the cosine impossible --> look at tan? 
 
 #we want dy/dx, so we want y2-y1/x2-x1 --> shoulder2-elbow2/shoulder1-elbow1
-line_ba = np.array(shou) - np.array(elb_new)
+#So in the attached, for angle 1, you want to do do atan2(y,x) 
+#where y=By-Ay and x=Bx-Ax. Ay Bx etc mean the y coordinate of point A, 
+#and Bx the x coordinate of B etc.
 
-def give_average(vids_list,dataframe):
-	return pd.mean[][][][]
+def segment_angle_func(body1,body2):	
+	#pull out body coordinates
+	x_1 = body1.iloc[:,0]
+	y_1 = body1.iloc[:,1]
+	x_2 = body2.iloc[:,0]
+	y_2 = body2.iloc[:,1]
 
-for val in vids_list:
-	print(val)
-	average = give_average(val,dataframe)
+	#find differences between the vertices
+	dy = (y_2-y_1)
+	dx = (x_2-x_1)
+
+	#calculate the angle in radians
+	angle = np.arctan2(dy,dx)
+
+	#this is in radian
+	#print(angle)
+
+	#change to degree
+	def to_degree(angle):
+		return np.rad2deg(angle)
+
+	#perform radians --> degree change and show
+	return to_degree(angle)
+
+seg_colshou = segment_angle_func(col,shou)
+seg_shouelb = segment_angle_func(shou,elb)
+seg_elbwri = segment_angle_func(elb,wri)
 
 
-#we should have max y and average y's per video but we can also group these together
-#somehow... but currently we cant because we dont have the true labels of these animals 
+#we should probably take the max and min values of these rats maybe as well?
+#the more features we can pull then the better for our clusters
+def mean(data):
+	return ((sum(data))/len(data))
 
+#output the mean degree value
+mean_seg_colshou = mean(seg_colshou)
+mean_seg_shouelb = mean(seg_shouelb)
+mean_seg_elbwri = mean(seg_elbwri)
 
+#we should also reference the reach window and use those frames as an index too 
+#maybe at that point we can use those to calc the mean/speed values and be like very official 
 
 
 
